081a73f3a6356ea62d4444e4c7b76b3156d5aff5
Zahra Kaboodvandi Rad <118894293+Zahra-Rad@users.noreply.github.com>
2025-04-29 15:22:31 +0330

    Create Pagination.tsx

---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,12 @@
+interface PaginationProps {
+  currentPage: number;
+  totalPages: number;
+  onPageChange: (page: number) => void;
+}
+
+export default function Pagination({ 
+  currentPage, 
+  totalPages, 
+  onPageChange 
+}: PaginationProps) {
+} 
